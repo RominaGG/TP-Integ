cecc95ae5942c233c55c6574822a9ec94b80b63c
Romina Gargano <rominaggargano@gmail.com>
2023-12-03 18:52:30 -0300

    creamos un formulario para propiedades

---
+++ Propieteer-Admin/js/view-propiedades.js
@@ -0,0 +1,20 @@
+const view_propiedades = Vue.createApp({
+    data(){
+        return{
+            ambientes:'',
+            descripcion:'',
+            direccion:'',
+            idPropiedad:'',
+            imagen:'',
+            mail_contacto:'',
+            precio:0,
+            propietario:'',
+            provincia:'',
+            tel_contacto:'',
+            url: 'https://marconu2697.pythonanywhere.com/propiedades'
+        }
+    },
+    methods: {
+
+    }
+})
