4d9364639d2e6ede25694abfcdb608367325472a
LeandroGastonLuna <lea21luna@gmail.com>
2023-12-03 23:53:52 -0300

    arregos (codigo deberia funcionar)

--- Propieteer-Admin/js/editar_vendedor.js
+++ Propieteer-Admin/js/editar_vendedor.js
@@ -1,54 +1,66 @@
-console.log(location.search)     // lee los argumentos pasados a este formulario
-var idVendedor=location.search.substr(12)  // producto_update.html?id=1
-console.log(idVendedor)
+console.log(location.search);     // lee los argumentos pasados a este formulario
+// location.search.substr(12)  // producto_update.html?id=1
+
+// console.log(location.search); // reads the arguments passed to this form
+const params = new URLSearchParams(location.search);
+const id = params.get('idVendedor'); // assuming the URL is like editarVendedor.html?idVendedor=1
+console.log(id);
+
 const { createApp } = Vue
-  createApp({
+createApp({
     data() {
-      return {
-        apellido:"",
-        idVendedor:0,
-        imagen:"",
-        mail:"",
-        matricula:"",
-        nombre:"",
-        profesion:"", 
-        sueldo:0,
-        telefono:0,
-        url:'https://marconu2697.pythonanywhere.com/vendedores/' + idVendedor,
-       }  
+        return {
+            apellido: "",
+            idVendedor: 0,
+            imagen: "",
+            mail: "",
+            matricula: "",
+            nombre: "",
+            profesion: "",
+            sueldo: 0,
+            telefono: 0,
+            url: 'https://marconu2697.pythonanywhere.com/vendedores/' + id,
+        }
     },
     methods: {
         fetchData(url) {
             fetch(url)
-                .then(response => response.json())
+                .then(response => {
+                    console.log(response);
+                    return response.json();
+                })
                 .then(data => {
-                    console.log(data)
-                    this.idVendedor=data.idVendedor
-                    this.apellido=data.apellido
-                    this.nombre=data.nombre
-                    this.matricula=data.matricula
-                    this.profesion=data.profesion
-                    this.mail=data.mail
-                    this.telefono=data.telefono
-                    this.sueldo=data.sueldo
-                    this.imagen=data.imagen
-                                        
+                    console.log('API Response:', data);
+                    if (Object.keys(data).length === 0) {
+                        // Handle empty response, e.g., display a message
+                        console.log('Empty response received');
+                    } else {
+                        console.log(data)
+                        this.apellido = data.apellido
+                        this.nombre = data.nombre
+                        this.matricula = data.matricula
+                        this.profesion = data.profesion
+                        this.mail = data.mail
+                        this.telefono = data.telefono
+                        this.sueldo = data.sueldo
+                        this.imagen = data.imagen
+                    }
                 })
                 .catch(err => {
                     console.error(err)
-                    this.error=true              
+                    this.error = true
                 })
         },
-        modificar(){
+        modificar() {
             let vendedor = {
-                apellido:this.apellido,
-                nombre:this.nombre,
-                matricula:this.matricula,
-                profesion:this.profesion,
-                mail:this.mail,
-                telefono:this.telefono,
+                apellido: this.apellido,
+                nombre: this.nombre,
+                matricula: this.matricula,
+                profesion: this.profesion,
+                mail: this.mail,
+                telefono: this.telefono,
                 sueldo: this.sueldo,
-                imagen:this.imagen
+                imagen: this.imagen
             }
             var options = {
                 body: JSON.stringify(vendedor),
@@ -59,16 +71,16 @@ const { createApp } = Vue
             fetch(this.url, options)
                 .then(function () {
                     alert("Registro modificado")
-                    window.location.href = "./views-code-tests.html"; 
+                    window.location.href = "./views-code-tests.html";
                 })
                 .catch(err => {
                     console.error(err);
                     alert("Error al Modificar")
-                })      
+                })
         }
 
     },
     created() {
         this.fetchData(this.url)
     },
-  }).mount('#appeditar')
+}).mount('#appeditar')
