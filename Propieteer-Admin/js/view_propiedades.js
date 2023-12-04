3773f1af1adc2a3a98dfdf74c2c5438e10362cdd
Romina Gargano <rominaggargano@gmail.com>
2023-12-03 23:43:06 -0300

    cambie el metodo grabar

--- Propieteer-Admin/js/view_propiedades.js
+++ Propieteer-Admin/js/view_propiedades.js
@@ -3,7 +3,7 @@ const propiedadesView = Vue.createApp({
     return {
       propiedades: [],
       // si el backend esta corriendo local  usar localhost 5000(si no lo subieron a pythonanywhere)
-       // si ya lo subieron a pythonanywhere
+      url: "https://marconu2697.pythonanywhere.com/propiedades", // si ya lo subieron a pythonanywhere
       error: false,
       /*atributos para el guardar los valores del formulario */
       ambientes: "",
@@ -16,7 +16,7 @@ const propiedadesView = Vue.createApp({
       precio: 0,
       tel_contacto: "",
       provincia: "",
-      url: "https://marconu2697.pythonanywhere.com/propiedades"
+      
     };
   },
   methods: {
@@ -47,7 +47,7 @@ const propiedadesView = Vue.createApp({
         });
     },
 
-    grabar() {
+   /* grabar() {
       let propiedad = {
         ambientes: this.ambientes,
         descripcion: this.descripcion,
@@ -75,7 +75,55 @@ const propiedadesView = Vue.createApp({
           console.error(err);
           alert("Error al Grabar"); // puedo mostrar el error tambien
         });
+    },*/
+    grabar() {
+      let propiedad = {
+        ambientes: this.ambientes,
+        descripcion: this.descripcion,
+        direccion: this.direccion,
+        imagen: this.imagen,
+        mail_contacto: this.mail_contacto,
+        precio: this.precio,
+        propietario: this.propietario,
+        provincia: this.provincia,
+        tel_contacto: this.tel_contacto,
+        // agrear los campos de la tabla (datos)
+      };
+    
+      var options = {
+        body: JSON.stringify(propiedad),
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        redirect: "follow",
+      };
+    
+      fetch(this.url, options)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
+        })
+        .then(data => {
+          // Manejar la respuesta exitosa de la API aquí
+          alert("Registro grabado exitosamente");
+          console.log(data); // Mostrar la respuesta de la API en la consola para verificar
+        })
+        .catch(error => {
+          console.error('There was an error!', error);
+          alert("Error al grabar el registro. Consulta la consola para más detalles.");
+        });
     },
+    
+  
+  
+  
+  
+  
+  
+  
+  
+  
   },
   created() {
     this.fetchData(this.url);
